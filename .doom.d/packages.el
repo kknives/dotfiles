be9c6e3016dbb29092d3474d0ce053f3fdc90dbf
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-08-03 12:02:48 +0530

    Replacce doom-snippets with yas

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -11,6 +11,8 @@
 (package! mixed-pitch)
 (package! arduino-mode)
 
+(package! doom-snippets :ignore t)
+(package! yasnippet-snippets)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
